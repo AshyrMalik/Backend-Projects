89a1d599f2b0cf2f4dec5e6f908957b8936ea4bc
Ashar <malikashar898@gmail.com>
2025-02-16 13:41:07 +0500

    Initial commit: set up monorepo for Node projects

---
+++ Node Starter/index.js
@@ -0,0 +1,8 @@
+fs = require("fs")
+
+textin=fs.readFileSync("./txt/input.txt","utf-8")
+console.log(textin)
+
+text_out= `This is what we know about: ${textin}`
+fs.writeFileSync("./txt/output.txt",text_out)
+console.log("output written to the file ")
